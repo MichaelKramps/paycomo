7aa96c6c21a148da85fde65ddff7d7a0f829c4d0
Michael Kramps <mdk989@gmail.com>
2018-10-18 07:46:30 -0500

    added sha256 method, might not be working

--- paycomo-service/src/main/java/com/paycomo/authorize/XPayToken.java
+++ paycomo-service/src/main/java/com/paycomo/authorize/XPayToken.java
@@ -1,6 +1,10 @@
 package com.paycomo.authorize;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 public class XPayToken {
     public String generateXPayToken(String urlPath, Object body, String sharedKey){
@@ -14,7 +18,12 @@ public class XPayToken {
         return token;
     }
 
-    private String sha256_HMAC(String key, String payload){
-        return "";
+    private String sha256_HMAC(String key, String message){
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+
+        String hash = Base64.encodeBase64String(sha256_HMAC.doFinal(message.getBytes()));
+        return hash;
     }
 }
